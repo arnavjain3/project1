23ae1b30821a4ed59b9903c967ddfcf0147953fe
arnavjain3 <98796234+arnavjain3@users.noreply.github.com>
2022-02-01 18:48:26 +0530

    Add files via upload

---
+++ GK QUIZ.py
@@ -0,0 +1,10 @@
+# This is a calculator made by Arnav Jain.
+answer1 = input("How many faces of US Presidents are carvedon Mount Rushmore?\n A) 5\n B) 3\nC) 7\nD) 4")
+
+if answer1 == "D":
+    print("You got it right")
+
+else:
+    print("Sorry, It is the wrong answer.The correct answer is D")
+
+
